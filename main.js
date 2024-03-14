c4efef921c86c350da9bc415a1d0ec5d74f6e441
Euchex <111068143+Euchex@users.noreply.github.com>
2024-03-14 16:13:29 +0100

    Added JS and some media querry

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ let nemuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-menuIcon.classList.toggle('fa-xmark');
-navbar.classList.toggle('active')
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
 }
 
 // scroll section active link
@@ -15,15 +15,15 @@ let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
 sections.forEach(sec => {
-let top = window.scrollY;
-let offset = sec.offsetTop - 150;
-let height = sec.offsetHeight;
-let id = sec.getAttribute('id');
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id');
 
-if(top >= offset && top < offset + height) {
-navLinks.forEach.apply(links => {
-links.classList.remove('active');
-document.querySelector('header nav a[href *= ' + id + ']').classList.add('active');
+    if(top >= offset && top < offset + height) {
+        navLinks.forEach.apply(links => {
+            links.classList.remove('active');
+            document.querySelector('header nav a[href *= ' + id + ']').classList.add('active');
 });
 
 };
@@ -51,9 +51,9 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'buttom' });
-ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-img, .service-container, .port-box, .contact form', { origin: 'buttom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 // Typed JS
 
