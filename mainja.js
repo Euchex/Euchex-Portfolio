c4d73fe2487c8bfab66232ee3277c3e46a4495d1
Euchex <euchexconcept@gmail.com>
2026-04-07 10:03:33 +0100

    Changed the JS script to

---
+++ mainja.js
@@ -0,0 +1,40 @@
+// Toggle icon navbar
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active')
+}
+
+// Scroll section active link
+
+let section = document.querySelectorAll('section');
+letnavLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach.apply(links => {
+                links.classList.remove('active');
+                document.querySelector('headr nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // Sticky navbar
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // Remove toggle icon and navbar
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
